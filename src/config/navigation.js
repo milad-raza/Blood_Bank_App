28a57eac23ee6fbf789f31b0f766af5c6713e222
milad-raza <miladrazabhatti@gmail.com>
2021-01-21 23:48:01 +0500

    signup added

--- src/config/navigation.js
+++ src/config/navigation.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Signup from '../screens/Signup';
 import Login from '../screens/Login';
@@ -11,9 +11,31 @@ function AppNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
-        <Stack.Screen name="Sign Up" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Sign Up"
+          component={Signup}
+          options={{
+            headerTintColor: '#ffffff',
+            headerStyle: {
+              backgroundColor: '#E6233F',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerTintColor: '#ffffff',
+            headerStyle: {
+              backgroundColor: '#E6233F',
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
